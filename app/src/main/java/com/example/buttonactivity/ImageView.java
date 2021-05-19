4e6987e0c79a2214bcac62fa71f2e5340b07478d
viram87 <viramchoksi77166@gmail.com>
2021-05-19 11:29:29 +0530

    added image view

---
+++ app/src/main/java/com/example/buttonactivity/ImageView.java
@@ -0,0 +1,14 @@
+package com.example.buttonactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_view);
+    }
+}
